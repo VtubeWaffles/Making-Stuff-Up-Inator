ece58afef46b7e5f1dc9a08687844748731bc750
VtubeWaffles <vtubewaffles@gmail.com>
2023-09-12 14:07:40 +0800

    Added the files

---
+++ making it up.py
@@ -0,0 +1,27 @@
+import random
+
+
+def read_paragraphs(file_path, encoding='utf-8'):
+	with open(file_path, 'r', encoding=encoding, errors='replace') as file:
+		paragraphs = file.read().split('\n\n')
+	return paragraphs
+
+def select_random_paragraphs(paragraphs, num_paragraphs):
+	return random.sample(paragraphs, num_paragraphs)
+
+def write_paragraphs_to_file(selected_paragraphs, file_path):
+        with open(file_path, 'w', encoding='utf-8') as file:
+                file.write('\n\n'.join(selected_paragraphs))
+
+source_file = 'source.txt'
+destination_file = 'destination.txt'
+
+num_random_paragraphs = 10
+
+paragraphs = read_paragraphs(source_file)
+
+selected_paragraphs = select_random_paragraphs(paragraphs, num_random_paragraphs)
+
+write_paragraphs_to_file(selected_paragraphs, destination_file)
+
+print(f"{num_random_paragraphs} paragraphs have been copied to the file.")
